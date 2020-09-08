b350216034aefde04ba0e101ec58c2a014500c17
Jake Waksbaum <jake.waksbaum@gmail.com>
2020-09-08 10:47:44 -0400

    Move shellcheck to checkPhase and disable check
    There seem to be issues with ghc on aarch64, so this makes it easy to
    enable/disable the use of shellcheck, which is written in Haskell.

--- nixos/modules/system/boot/loader/raspberrypi/raspberrypi-builder.nix
+++ nixos/modules/system/boot/loader/raspberrypi/raspberrypi-builder.nix
@@ -4,6 +4,10 @@ pkgs.substituteAll {
   src = ./raspberrypi-builder.sh;
   isExecutable = true;
 
+  checkPhase = "shellcheck $out";
+  checkInputs = [ pkgs.buildPackages.shellcheck ];
+  doCheck = false;
+
   inherit (pkgs) bash;
   path = [pkgs.coreutils pkgs.gnused pkgs.gnugrep];
   inherit firmware configTxt;
